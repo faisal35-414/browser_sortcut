b8a6051de91a33b3504ea868756f72539ec0c2c9
Faisal Mahmud <111185647+faisal35-414@users.noreply.github.com>
2022-08-13 16:06:03 -0400

    Update new.py

--- new.py
+++ new.py
@@ -1,45 +1,60 @@
-name = input("What's ypur name?\n")
 
-print("Welcome " + name + ", select your option")
+name = input("What's your name?\n")
 
-print("""
-      1. Github
-      2. Email
-      3. Facebook
-      4. Youtube""")
+if name == "faisal":
+    print("Welcome " + name + ", select your option")
 
 
-
-number = input()
+    i = 0
+  
+ # Using while loop
+    while True:
+        if i==100:
+            break
+        print("""
+        1. Github
+        2. Email
+        3. Facebook
+        4. Youtube
+        5. exit """)
+        
+        number = input()
     
-if number == "1":
-   print("Open git")
+        if number == "1":
+            print("Open git")
     
-   import webbrowser
-   get_url= webbrowser.open('https://github.com/')
+            import webbrowser
+            get_url= webbrowser.open('https://github.com/')
 
 
-elif number == "2":
-    print("Open Email")
-
-    import webbrowser
-    get_url= webbrowser.open('https://mail.google.com/')
-
-
-
-elif number == "3":
-    print("open facebook")
+        elif number == "2":
+            print("Open Email")
     
-    import webbrowser
-    get_url= webbrowser.open('https://www.facebook.com/')
+            import webbrowser
+            get_url= webbrowser.open('https://mail.google.com/')
 
-elif number == "4":
-    print("open youtube")
+
+        elif number == "3":
+            print("Open facebook")
+            
+            import webbrowser
+            get_url= webbrowser.open('https://www.facebook.com/')
+
+        elif number == "4":
+            print("Open youtube")
     
-    import webbrowser
-    get_url= webbrowser.open('https://www.youtube.com/')
+            import webbrowser
+            get_url= webbrowser.open('https://www.youtube.com/')
  
+        elif number == "5":
+            break
+
+        else : 
+            print("Please write right word")
+
+    i += 1
+else :
+    print("Please write right name")
 
 
-else : 
-    print("please write right word")
+    
